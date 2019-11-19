c5ad4c461195194ad2544309e46f1eda39b592b8
Lucas Ferreira <ferreira.lucas639@gmail.com>
2019-11-19 18:13:46 -0300

    tela de login

--- altaImpedancia/urls.py
+++ altaImpedancia/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
 
     path('', views.loginPage, name='login'),
+    path('submit',views.login_submit,  name='submit'),
+    path('logado/',views.index,  name='index'),
+    path('logout/',views.logout_user,  name='logout'),
 
 ]
--- altaImpedancia/views.py
+++ altaImpedancia/views.py
@@ -9,4 +9,36 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 
 def loginPage(request):
-    return render(request)
+    if request.session._session:
+        return redirect('/logado/')
+    else:
+        return render(request,'login.html')
+
+@csrf_protect   
+def login_submit(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+           login(request, user)
+          
+           return redirect('/logado/')
+        else:
+            messages.error(request,'Usuário ou senha invalidos')
+    return redirect('/')
+
+def index(request):
+    if request.session._session:
+        return render(request,'home.html')
+    else:
+        messages.error(request, 'Você precisa estar logado!')
+        return redirect('/')
+
+def logout_user(request):
+    
+    logout(request)
+
+    return redirect('/')
+
