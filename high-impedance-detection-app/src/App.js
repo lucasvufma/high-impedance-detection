0d3effa0340dd41836f4bb40c09ac49397371494
Lucas <lucasvufma@gmail.com>
2019-11-20 16:18:19 -0300

    Login Page and Routers

--- high-impedance-detection-app/src/App.js
+++ high-impedance-detection-app/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Switch from '@material-ui/core/Switch';
-import Paper from '@material-ui/core/Paper';
-import Slide from '@material-ui/core/Slide';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import InteractiveMap from './components/interactiveMap'
-
+import InteractiveMap from './components/InteractiveMap/interactiveMap'
+import './App.css';
+import LoginPage from './components/Login/login';
+import Home from './components/HomePage/homePage';
+import { useState, useEffect } from 'react'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline, CircularProgress } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
 
@@ -16,9 +15,13 @@ import InteractiveMap from './components/interactiveMap'
 function App(){
   
   return (
-  <div > 
-    <InteractiveMap />
-  </div>
+   
+			<Router>
+				<Switch>
+					<Route exact path="/" component={LoginPage} />   
+          <Route exact path="/home" component={Home} />      
+				</Switch>
+			</Router>
   );
   }
 
---
+++ high-impedance-detection-app/src/components/HomePage/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import InteractiveMap from '../InteractiveMap/interactiveMap'
+
+export default function Home(){
+    return(
+        <div>
+        <InteractiveMap />
+        </div>
+    );
+}
